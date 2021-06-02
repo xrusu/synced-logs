393a097dfdd13dc6442c197451459b15bcf70b4a
Rusu Dinu <dinu@codingshadows.com>
2021-06-02 20:46:05 +0300

    created the service to send the log data to the RESTapi server

--- lib/service/server_logs_service.dart
+++ lib/service/server_logs_service.dart
@@ -1,3 +1,37 @@
-import 'package:http/http.dart' as http;
+import 'dart:convert';
 
-class ServerLogsService {}
+import 'package:http/http.dart' as http;
+import 'package:synced_logs/data/runtime_data.dart';
+import 'package:synced_logs/model/synced_log.dart';
+
+class ServerLogsService {
+  static Future<int> saveLog(SyncedLog log) async {
+    if (RuntimeData.apiLogsServer.trim() != "" && RuntimeData.apiLogsEndpoint.trim() != "") {
+      final response = await http.post(
+        Uri.https(RuntimeData.apiLogsServer, RuntimeData.apiLogsEndpoint),
+        headers: <String, String>{
+          'Content-Type': 'application/json; charset=UTF-8',
+        },
+        body: jsonEncode(log.toJson()),
+      );
+      return response.statusCode;
+    } else {
+      throw ("Api server endpoints not set!");
+    }
+  }
+
+  static Future<int> saveBulkLog(List<SyncedLog> syncedLogs) async {
+    if (RuntimeData.apiLogsServer.trim() != "" && RuntimeData.apiBulkLogsEndpoint.trim() != "") {
+      final response = await http.post(
+        Uri.https(RuntimeData.apiLogsServer, RuntimeData.apiBulkLogsEndpoint),
+        headers: <String, String>{
+          'Content-Type': 'application/json; charset=UTF-8',
+        },
+        body: jsonEncode(syncedLogs),
+      );
+      return response.statusCode;
+    } else {
+      throw ("Api server endpoints not set!");
+    }
+  }
+}

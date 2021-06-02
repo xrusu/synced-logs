201c9d66a2ba689d3c8c2975a408a03edda7c2d2
Rusu Dinu <dinu@codingshadows.com>
2021-06-02 21:03:03 +0300

    updated comments to match dart best practices

--- lib/data/runtime_data.dart
+++ lib/data/runtime_data.dart
@@ -2,12 +2,12 @@ class RuntimeData {
   static bool initialized = false;
   static String logsFolder = "/synced_logs/";
 
-  // the domain of the server, for example: 'logsingest.codingshadows.com'
+  /// the domain of the server, for example: 'logsingest.codingshadows.com'
   static String apiLogsServer = "";
 
-  // send only one log at a time to ingest, for example 'api/save/log'
+  /// send only one log at a time to ingest, for example 'api/save/log'
   static String apiLogsEndpoint = "";
-  
-  // send a list of logs to ingest, for example 'api/save/bulk/log'
+
+  /// send a list of logs to ingest, for example 'api/save/bulk/log'
   static String apiBulkLogsEndpoint = "";
 }
